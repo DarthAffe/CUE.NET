d566b7b6f256227d2882d5abb7bd1b66ab8fdbcf
zlotap <zlotap@wyrez.org>
2015-09-22 20:45:45 +0200

    perperation for merge from HEAD

---
+++ Devices/Keyboard/ColorBrushes/AbstractGradientBrush.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+
+namespace CUE.NET.Devices.Keyboard.ColorBrushes
+{
+    abstract class AbstractGradientBrush : IBrush
+    {
+        #region Fields
+
+        private Point anchor;
+        //private Vect
+
+        #endregion
+
+        #region Methods
+
+        public abstract Color getColorAtPoint(Point point);
+
+        #endregion
+    }
+}
