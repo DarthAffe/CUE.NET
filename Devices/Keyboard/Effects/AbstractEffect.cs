b977edf1dae79ec1798d2c84726b3c72b0a95ea6
Darth Affe <darthaffe@wyrez.org>
2015-10-09 21:24:53 +0200

    Added FlashEffect

---
+++ Devices/Keyboard/Effects/AbstractEffect.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CUE.NET.Devices.Keyboard.Brushes;
+using CUE.NET.Devices.Keyboard.Keys;
+
+namespace CUE.NET.Devices.Keyboard.Effects
+{
+    public abstract class AbstractEffect : IEffect
+    {
+        #region Properties & Fields
+
+        public abstract IBrush EffectBrush { get; }
+
+        public IEnumerable<CorsairKey> KeyList { get; protected set; }
+
+        public bool IsDone { get; protected set; }
+
+        #endregion
+
+        #region Methods
+
+        public void SetTarget(IKeyGroup keyGroup)
+        {
+            KeyList = keyGroup.Keys.ToList();
+        }
+
+        public abstract void Update(float deltaTime);
+
+        public virtual void OnAttach()
+        { }
+
+        public virtual void OnDetach()
+        { }
+
+        #endregion
+    }
+}
