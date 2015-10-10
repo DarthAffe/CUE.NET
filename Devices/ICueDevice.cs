2590c4ad2a86d370a09985641577976eddbbeb2f
Darth Affe <darthaffe@wyrez.org>
2015-10-10 12:29:35 +0200

    Added pokemon exception handling around effect- and brush-updates to prevent issues with wrong implementations.

--- Devices/ICueDevice.cs
+++ Devices/ICueDevice.cs
@@ -1,14 +1,20 @@
-﻿using CUE.NET.Devices.Generic.Enums;
+﻿using CUE.NET.Devices.Generic;
+using CUE.NET.Devices.Generic.Enums;
 
 namespace CUE.NET.Devices
 {
+    public delegate void OnExceptionEventHandler(object sender, OnExceptionEventArgs args);
+
     public interface ICueDevice
     {
+
         IDeviceInfo DeviceInfo { get; }
 
         UpdateMode UpdateMode { get; set; }
 
         float UpdateFrequency { get; set; }
+
+        event OnExceptionEventHandler OnException;
 
         void Update(bool flushLeds = false);
     }
