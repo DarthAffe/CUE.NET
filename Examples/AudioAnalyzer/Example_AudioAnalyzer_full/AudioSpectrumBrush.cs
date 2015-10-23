6ed251a4844fa102d3139b1470b82243918f05e9
Darth Affe <darthaffe@wyrez.org>
2015-10-23 22:54:09 +0200

    Added AudioAnalyzer-Example

---
+++ Examples/AudioAnalyzer/Example_AudioAnalyzer_full/AudioSpectrumBrush.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Drawing;
+using CUE.NET.Brushes;
+using CUE.NET.Gradients;
+using Example_AudioAnalyzer_full.TakeAsIs;
+
+namespace Example_AudioAnalyzer_full
+{
+    // Extending from LinearGradientBrush since this is all we want to draw - of course you could implement everything on your own
+    public class AudioSpectrumBrush : LinearGradientBrush
+    {
+        #region Properties & Fields
+
+        public float[] BarData { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        // default values for start/end are fine
+        public AudioSpectrumBrush(IGradient gradient)
+            : base(gradient)
+        { }
+
+        #endregion
+
+        #region Methods
+
+        public override Color GetColorAtPoint(RectangleF rectangle, PointF point)
+        {
+            if (BarData == null) return Color.Transparent;
+
+            // This logic is also stolen from AterialDawn
+            int barSampleIndex = (int)Math.Floor(BarData.Length * (point.X / (rectangle.X + rectangle.Width))); // Calculate bar sampling index
+            float curBarHeight = 1f - Utility.Clamp(BarData[barSampleIndex], 0f, 1f); // Invert this value since the keyboard is laid out with topleft being point 0,0
+            float verticalPos = (point.Y / rectangle.Height);
+
+            // If the barHeight is lower than the vertical pos currently calculated return the brush value. Otherwise do nothing by returning transparent.
+            return curBarHeight <= verticalPos ? base.GetColorAtPoint(rectangle, point) : Color.Transparent;
+        }
+
+        #endregion
+    }
+}
---
+++ Examples/AudioAnalyzer/Example_AudioAnalyzer_full/TakeAsIs/WASAPIInitializationException.cs
@@ -0,0 +1,26 @@
+﻿/* 
+ * ###################################################################################################################
+ * #                                             Thanks to AterialDawn                                               #
+ * #                                                                                                                 #
+ * # Source: https://github.com/AterialDawn/CUEAudioVisualizer/blob/master/CUEAudioVisualizer/Exceptions/WASAPIInitializationException.cs  #
+ * #   Date: 23.10.2015                                                                                              #
+ * # Commit: d415fd1dda2e7fd98391ac83c000d7e4a781558e                                                                #
+ * #                                                                                                                 #
+ * ###################################################################################################################
+ * 
+ * > Note < This code is refactored and all parts not used inb this tutorial are removed.
+ */
+
+using System;
+using Un4seen.Bass;
+
+namespace Example_AudioAnalyzer_full.TakeAsIs
+{
+    public class WASAPIInitializationException : Exception
+    {
+        public BASSError? OptionalError { get; private set; }
+
+        public WASAPIInitializationException(string message) : base(message) { OptionalError = null; }
+        public WASAPIInitializationException(string message, BASSError error) : base(message) { OptionalError = error; }
+    }
+}
